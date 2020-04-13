3462adb0062074dfad860ae8fcc17a5290dc6cd6
SQL47 <ajidalmajid6@gmail.com>
2020-04-13 08:30:20 +0700

    home  index views

---
+++ app/views/home/index.php
@@ -0,0 +1,38 @@
+<div class="row-center">
+  <h1 class="border-bottom">Polling</h1>
+</div>
+<div class="row-box">
+  <div class="pool-card">
+    <h3>Laravel</h3>
+    <span class="text-suara">50 suara</span>
+    <div class="pool-bar">
+      <span class="span1">
+        <span class="span2" style="width:70%">
+          <span class="span3">1%</span>
+        </span>
+      </span>
+    </div>
+  </div>
+  <div class="pool-card">
+    <h3>CodeIgniter</h3>
+    <span class="text-suara">70 suara</span>
+    <div class="pool-bar">
+      <span class="span1">
+        <span class="span2" style="width:70%">
+          <span class="span3">70% Orang memilih CodeIgniter</span>
+        </span>
+      </span>
+    </div>
+  </div>
+  <div class="pool-card">
+    <h3>Symfony</h3>
+    <span class="text-suara">10 suara</span>
+    <div class="pool-bar">
+      <span class="span1">
+        <span class="span2" style="width:70%">
+          <span class="span3">0% Orang memilih Symfony</span>
+        </span>
+      </span>
+    </div>
+  </div>
+</div>
