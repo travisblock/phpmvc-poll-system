8df96267232335e2a1b482cf8462a24af23a5ecd
SQL47 <ajidalmajid6@gmail.com>
2020-04-15 00:08:35 +0700

    login user model

---
+++ app/models/loginUser.php
@@ -0,0 +1,37 @@
+<?php
+
+class loginUser{
+
+  private $db;
+
+  public function __construct(){
+    $this->db = Database::getDB();
+  }
+
+  public function cekUser($user){
+    if(!empty($user)){
+      $query = "SELECT * FROM login where user=:usr";
+      $this->db->query($query);
+      $this->db->bind('usr', $user);
+      $this->db->execute();
+      if($this->db->rowCount() > 0){
+        return $this->db->result();
+      }
+      return false;
+    }
+    return false;
+  }
+
+  public function login($user, $pass){
+    $password = $this->cekUser($user);
+    if(!empty($password)){
+      if(password_verify($pass, $password['pass'])){
+        return true;
+      }else{
+        return false;
+      }
+    }
+    return false;
+  }
+
+}
