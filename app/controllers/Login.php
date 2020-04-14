a5f3903cd11928eabb7fedb3a50bda964d55df21
SQL47 <ajidalmajid6@gmail.com>
2020-04-15 00:09:25 +0700

    add login

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -7,4 +7,22 @@ class Login extends Controller{
     $this->view('login/index');
 
   }
+
+  // public function cekUser($user){
+  //   $query = "SELECT * FROM login WHERE user=:user";
+  //   $this->db->query($query);
+  //   $this->db->bind('user', $user);
+  //   $this->db->execute();
+  //   var_dump($this->db);
+  //   var_dump($this->db->rowCount());
+  // }
+
+  public function masuk(){
+    $user = Input::get('user');
+    $pass = Input::get('pass');
+
+    var_dump($this->model('loginUser')->login($user, $pass));
+    
+  }
+
 }
