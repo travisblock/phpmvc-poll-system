0176263eb8a797f8f7a0f2ff721c671be1bfefac
SQL47 <ajidalmajid6@gmail.com>
2020-04-15 12:12:52 +0700

    user page

---
+++ app/views/user/index.php
@@ -0,0 +1,50 @@
+<div class="row-center">
+  <h1>Voting</h1>
+</div>
+<div class="heading-2">
+  <h2 class="border-bottom">Pilih pemain terbaik</h2>
+</div>
+<div class="row-box">
+  <div class="pool-card">
+    <h3>Laravel</h3>
+    <span class="text-suara">50 suara</span>
+    <div class="pool-bar">
+      <span class="span1">
+        <span class="span2" style="width:50%">
+          <span class="span3">50%</span>
+        </span>
+      </span>
+    </div>
+    <div class="tombol">
+      <button class="button">Detail</button>
+    </div>
+  </div>
+  <div class="pool-card">
+    <h3>CodeIgniter</h3>
+    <span class="text-suara">30 suara</span>
+    <div class="pool-bar">
+      <span class="span1">
+        <span class="span2" style="width:30%">
+          <span class="span3">30%</span>
+        </span>
+      </span>
+    </div>
+    <div class="tombol">
+      <button class="button">Detail</button>
+    </div>
+  </div>
+  <div class="pool-card">
+    <h3>Symfony</h3>
+    <span class="text-suara">20 suara</span>
+    <div class="pool-bar">
+      <span class="span1">
+        <span class="span2" style="width:20%">
+          <span class="span3">20%</span>
+        </span>
+      </span>
+    </div>
+    <div class="tombol">
+      <button class="button">Detail</button>
+    </div>
+  </div>
+</div>
