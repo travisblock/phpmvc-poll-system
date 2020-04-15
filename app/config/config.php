3ff9e0736726a5b5069c638154b3d80c926fb14c
SQL47 <ajidalmajid6@gmail.com>
2020-04-15 12:13:14 +0700

    modified base url

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASEURL', 'http://localhost:8081');
+define('BASEURL', 'http://192.168.1.13:8081');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
