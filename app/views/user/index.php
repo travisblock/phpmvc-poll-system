346689aa4b978e8dd4580c41d098ee24f4ee610e
SQL47 <ajidalmajid6@gmail.com>
2020-04-16 11:05:20 +0700

    add modal

--- app/views/user/index.php
+++ app/views/user/index.php
@@ -16,7 +16,7 @@
       </span>
     </div>
     <div class="tombol">
-      <button class="button">Detail</button>
+      <label for="click" class="button">Detail</label>
     </div>
   </div>
   <div class="pool-card">
@@ -30,7 +30,7 @@
       </span>
     </div>
     <div class="tombol">
-      <button class="button">Detail</button>
+      <label for="click" class="button">Detail</label>
     </div>
   </div>
   <div class="pool-card">
@@ -44,7 +44,24 @@
       </span>
     </div>
     <div class="tombol">
-      <button class="button">Detail</button>
+      <label for="click" class="button">Detail</label>
     </div>
   </div>
+</div>
+
+
+<input type="checkbox" id="click">
+<!-- <label for="click" class="click-me">Click Me</label> -->
+<div class="modal-content">
+  <div class="header">
+    <h2>Pilih Calon</h2>
+    <label for="click" class="close">
+      <svg height="30px" viewBox="0 0 365 365" width="30px" xmlns="http://www.w3.org/2000/svg"><path d="m243.1875 182.859375 113.132812-113.132813c12.5-12.5 12.5-32.765624 0-45.246093l-15.082031-15.082031c-12.503906-12.503907-32.769531-12.503907-45.25 0l-113.128906 113.128906-113.132813-113.152344c-12.5-12.5-32.765624-12.5-45.246093 0l-15.105469 15.082031c-12.5 12.503907-12.5 32.769531 0 45.25l113.152344 113.152344-113.128906 113.128906c-12.503907 12.503907-12.503907 32.769531 0 45.25l15.082031 15.082031c12.5 12.5 32.765625 12.5 45.246093 0l113.132813-113.132812 113.128906 113.132812c12.503907 12.5 32.769531 12.5 45.25 0l15.082031-15.082031c12.5-12.503906 12.5-32.769531 0-45.25zm0 0"/></svg>
+    </label>
+  </div>
+  <div class="isi">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex incidunt, necessitatibus minima sit. Architecto aperiam quam voluptate, esse ut iste nisi cumque adipisci, quod laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex incidunt, necessitatibus minima sit. Architecto aperiam quam voluptate, esse ut iste nisi cumque adipisci, quod laborum. Ex incidunt, necessitatibus minima sit. </p>
+  </div>
+  <div class="line"></div>
+  <label for="click" class="close-btn">Close</label>
 </div>
