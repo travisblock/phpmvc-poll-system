7bb7679416c43b33b801e20d7fe0c46feb77e564
SQL47 <ajidalmajid6@gmail.com>
2020-04-18 22:00:28 +0700

    fix input to database

--- app/models/registerUser.php
+++ app/models/registerUser.php
@@ -2,9 +2,36 @@
 
 class registerUser{
 
+  private $db;
+
+  public function __construct(){
+    return $this->db = Database::getDB();
+  }
+
   public function register($data = array()){
     if($data){
-      return $data;
+      $query = "INSERT INTO user(username,email,pass) VALUES(:username, :email, :pass)";
+      $this->db->query($query);
+      $this->db->bind('username', $data['user']);
+      $this->db->bind('email', $data['email']);
+      $this->db->bind('pass', $data['pass']);
+      $this->db->execute();
+
+      return ($this->db->rowCount() > 0) ? true : false;
+    }else{
+      return null;
+    }
+  }
+
+  public function cekRegister($data = array()){
+    if($data){
+      $query = "SELECT * FROM user WHERE username=:username OR email=:email";
+      $this->db->query($query);
+      $this->db->bind('username', $data['user']);
+      $this->db->bind('email', $data['email']);
+      $this->db->execute();
+
+      return ($this->db->rowCount() > 0) ? false : true;
     }else{
       return null;
     }
