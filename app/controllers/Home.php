a346c6b7a6d1b8648cd559f3f42c9d92c25547ad
SQL47 <ajidalmajid6@gmail.com>
2020-04-20 22:53:59 +0700

    add polling to home index

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,9 +3,9 @@
 class Home extends Controller{
   public function index(){
     $data['judul'] = 'Sistem Polling Realtime';
-
+    $data['poll'] = $this->model('PollingUser')->getPolling();
     $this->view('templates/header', $data);
-    $this->view('home/index');
+    $this->view('home/index', $data);
     $this->view('templates/footer');
   }
 }
