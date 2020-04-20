9fac9e7cf02b845a492319b282cbdd13af9e291e
SQL47 <ajidalmajid6@gmail.com>
2020-04-20 23:08:14 +0700

    update menubar

--- public/js/menubar.js
+++ public/js/menubar.js
@@ -1,6 +1,14 @@
-$(document).ready(function(){
+// $(document).ready(function(){
+//
+// $('.menu-icon').on("click", function(){
+//   $("nav ul").toggleClass("showing");
+// });
+// });
 
-$('.menu-icon').on("click", function(){
-  $("nav ul").toggleClass("showing");
-});
-});
+
+var menu = document.querySelector(".menu-icon");
+
+menu.onclick = function(){
+  var nav = document.querySelector('nav ul');
+  nav.classList.toggle('showing');
+};
