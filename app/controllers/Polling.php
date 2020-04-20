248f081411a83bea3b74cb9315c4e84dd8b972b6
SQL47 <ajidalmajid6@gmail.com>
2020-04-20 22:54:22 +0700

    polling fetch via ajax

--- app/controllers/Polling.php
+++ app/controllers/Polling.php
@@ -1,7 +1,28 @@
 <?php
 
-class Polling{
+class Polling extends Controller{
+
   public function index(){
-    echo "A";
+
   }
+
+  public function getPollingById(){
+      $id = json_decode(file_get_contents('php://input'), true);
+      if(!is_null($id)){
+        foreach($id as $key){
+          $data = $this->model('PollingUser')->getPollById($key);
+          $this->view('user/polling', $data);
+        }
+      }
+  }
+
+  public function pilih(){
+    $id = Input::get('idPool');
+    if($this->model('PollingUser')->pollInput($id) > 0){
+      header('Location:'. BASEURL);
+    }else{
+      header('Location:'. BASEURL . '/user/home');
+    }
+  }
+  
 }
