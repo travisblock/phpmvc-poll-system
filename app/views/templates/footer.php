6770bcaf01a61d62c41cbb77a218ae45f6927d92
SQL47 <ajidalmajid6@gmail.com>
2020-04-20 22:51:50 +0700

    remove jquery and add script.js

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,6 +1,6 @@
 </div>
 </div>
-<script type="text/javascript" src="<?= BASEURL; ?>/public/js/jquery.min.js"></script>
+<script type="text/javascript" src="<?= BASEURL; ?>/public/js/script.js"></script>
 <script type="text/javascript" src="<?= BASEURL; ?>/public/js/menubar.js"></script>
 </body>
 </html>
