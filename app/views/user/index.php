4fdf71a70eecd1ddef84acc31e85076250e5da4e
SQL47 <ajidalmajid6@gmail.com>
2020-04-20 22:52:08 +0700

    fix user view

--- app/views/user/index.php
+++ app/views/user/index.php
@@ -4,64 +4,32 @@
 <div class="heading-2">
   <h2 class="border-bottom">Pilih pemain terbaik</h2>
 </div>
-<div class="row-box">
+<div class="row-box" id="pollData">
+  <?php
+  foreach($data['poll'] as $key){
+  ?>
   <div class="pool-card">
-    <h3>Laravel</h3>
-    <span class="text-suara">50 suara</span>
+    <h3><?= $key['name']; ?></h3>
+    <span class="text-suara"><?= $key['value']; ?> suara</span>
     <div class="pool-bar">
       <span class="span1">
-        <span class="span2" style="width:50%">
-          <span class="span3">50%</span>
+        <span class="span2" style="width:<?= $key['value']; ?>%">
+          <span class="span3"><?= $key['value']; ?>%</span>
         </span>
       </span>
     </div>
     <div class="tombol">
-      <label for="click" class="button">Detail</label>
+      <label for="click" class="button detailBtn" data-id="<?= $key['id']; ?>">Detail</label>
     </div>
   </div>
-  <div class="pool-card">
-    <h3>CodeIgniter</h3>
-    <span class="text-suara">30 suara</span>
-    <div class="pool-bar">
-      <span class="span1">
-        <span class="span2" style="width:30%">
-          <span class="span3">30%</span>
-        </span>
-      </span>
-    </div>
-    <div class="tombol">
-      <label for="click" class="button">Detail</label>
-    </div>
-  </div>
-  <div class="pool-card">
-    <h3>Symfony</h3>
-    <span class="text-suara">20 suara</span>
-    <div class="pool-bar">
-      <span class="span1">
-        <span class="span2" style="width:20%">
-          <span class="span3">20%</span>
-        </span>
-      </span>
-    </div>
-    <div class="tombol">
-      <label for="click" class="button">Detail</label>
-    </div>
-  </div>
+  <?php
+  }
+  ?>
 </div>
 
 
 <input type="checkbox" id="click">
 <!-- <label for="click" class="click-me">Click Me</label> -->
-<div class="modal-content">
-  <div class="header">
-    <h2>Pilih Calon</h2>
-    <label for="click" class="close">
-      <svg height="30px" viewBox="0 0 365 365" width="30px" xmlns="http://www.w3.org/2000/svg"><path d="m243.1875 182.859375 113.132812-113.132813c12.5-12.5 12.5-32.765624 0-45.246093l-15.082031-15.082031c-12.503906-12.503907-32.769531-12.503907-45.25 0l-113.128906 113.128906-113.132813-113.152344c-12.5-12.5-32.765624-12.5-45.246093 0l-15.105469 15.082031c-12.5 12.503907-12.5 32.769531 0 45.25l113.152344 113.152344-113.128906 113.128906c-12.503907 12.503907-12.503907 32.769531 0 45.25l15.082031 15.082031c12.5 12.5 32.765625 12.5 45.246093 0l113.132813-113.132812 113.128906 113.132812c12.503907 12.5 32.769531 12.5 45.25 0l15.082031-15.082031c12.5-12.503906 12.5-32.769531 0-45.25zm0 0"/></svg>
-    </label>
-  </div>
-  <div class="isi">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex incidunt, necessitatibus minima sit. Architecto aperiam quam voluptate, esse ut iste nisi cumque adipisci, quod laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex incidunt, necessitatibus minima sit. Architecto aperiam quam voluptate, esse ut iste nisi cumque adipisci, quod laborum. Ex incidunt, necessitatibus minima sit. </p>
-  </div>
-  <div class="line"></div>
-  <label for="click" class="close-btn">Close</label>
+<div class="modal-content" id="pollDetail">
+
 </div>
