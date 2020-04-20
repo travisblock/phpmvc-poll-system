32159050892aff94d5ae74e8a00350ce5b7bb944
SQL47 <ajidalmajid6@gmail.com>
2020-04-20 23:08:35 +0700

    delete commentar

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", function(){
 
-  // var xhr = new XMLHttpRequest();
-  // var url = 'http://192.168.1.13:8081/polling/getPolling';
-  //
-  // xhr.onreadystatechange = function(){
-  //   if(this.readyState == 4 && this.status == 200){
-  //     document.getElementById("pollData").innerHTML = this.responseText;
-  //   }
-  // };
-  //
-  // xhr.open("get", url, true);
-  // xhr.send();
-
   var detail = document.getElementsByClassName("detailBtn");
 
   for (var i=0; i < detail.length; i++){
@@ -31,16 +19,6 @@ document.addEventListener("DOMContentLoaded", function(){
       };
       var data = {id:id};
       xhr.send(JSON.stringify(data));
-
-      // $.ajax({
-      //   url: 'http://192.168.1.13:8081/polling/getPollingById',
-      //   data: {id : id},
-      //   method: 'post',
-      //   dataType: 'json',
-      //   success: function(data){
-      //     $('#pollDetail').html(data);
-      //   }
-      // });
 
     };
   };
