949c06ce8578651d6a218766b56b453bac8e974c
SQL47 <ajidalmajid6@gmail.com>
2020-04-21 20:30:42 +0700

    add update : insert into table done

--- app/models/PollingUser.php
+++ app/models/PollingUser.php
@@ -22,11 +22,28 @@ class PollingUser{
     return $this->db->result();
   }
 
-  public function pollInput($id){
-    $query = "UPDATE polling SET value=value+1 WHERE id=:id";
-    $this->db->query($query);
+  public function pollInput($id = null, $user = null){
+    if(!is_null($id) && !is_null($user)){
+      $query = "UPDATE polling SET value=value+1 WHERE id=:id";
+      $this->db->query($query);
+      $this->db->bind('id', $id);
+      $this->db->execute();
+      var_dump($this->catatUser($user));
+      return $this->db->rowCount();
+    }
+  }
+
+  public function catatUser($user){
+    $query_id = "SELECT id FROM user WHERE username=:user";
+    $this->db->query($query_id);
+    $this->db->bind('user', $user);
+    $id = $this->db->result();
+    $id = $this->db->rowCount() > 0 ? $id['id'] : null;
+
+    $query_catat = "UPDATE user set sudah=sudah+1 WHERE id=:id";
+    $this->db->query($query_catat);
     $this->db->bind('id', $id);
     $this->db->execute();
-    return $this->db->rowCount();
+    return ($this->db->rowCount() > 0) ? true : null;
   }
 }
