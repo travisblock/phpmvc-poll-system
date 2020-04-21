ac7526c93c80a80c2f31c8f726aebf5d15ff914f
SQL47 <ajidalmajid6@gmail.com>
2020-04-21 20:29:04 +0700

    modified user controller

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -3,8 +3,13 @@
 class User extends Controller{
 
   public function index(){
-
-    $this->view('login/index');
+    session_start();
+    if(empty(Session::get('username'))){
+      $this->view('login/index');
+    }else{
+      header('Location:'. BASEURL .'/user/home');
+      exit();
+    }
 
   }
 
@@ -50,21 +55,31 @@ class User extends Controller{
   }
 
   public function register(){
-    $data['email'] = htmlentities(Input::get('email'));
-    $data['user']  = htmlentities(Input::get('user'));
-    $data['pass']  = password_hash(Input::get('pass'), PASSWORD_DEFAULT);
+    session_start();
+    if(empty(Session::get('username'))){
+      $data['email'] = htmlentities(Input::get('email'));
+      $data['user']  = htmlentities(Input::get('user'));
+      $data['pass']  = password_hash(Input::get('pass'), PASSWORD_DEFAULT);
+      if(!empty($data['user']) && !empty($data['email'])){
+        if($this->model('registerUser')->cekRegister($data)){
+          if($this->model('registerUser')->register($data)){
 
-    if($this->model('registerUser')->cekRegister($data)){
-      if($this->model('registerUser')->register($data)){
-        session_start();
-        Session::set('username', $data['user']);
-        header('Location:'. BASEURL .'/user/home');
-        exit();
+            Session::set('username', $data['user']);
+            header('Location:'. BASEURL .'/user/home');
+            exit();
+          }else{
+            echo "<script>alert('Ada kesalahan saat input data'); window.location.href='".BASEURL."/user'</script>";
+          }
+        }else{
+          echo "<script>alert('Username / email sudah digunakan'); window.location.href='".BASEURL."/user'</script>";
+        }
       }else{
-        echo "<script>alert('Ada kesalahan saat input data'); window.location.href='".BASEURL."/user'</script>";
+        header('Location:'. BASEURL . '/user');
+        exit();
       }
     }else{
-      echo "<script>alert('Username / email sudah digunakan'); window.location.href='".BASEURL."/user'</script>";
+      header('Location:'. BASEURL .'/user/home');
+      exit();
     }
   }
 
