7e8e5d277a57794e9b819161c1273214e1467961
SQL47 <ajidalmajid6@gmail.com>
2020-04-22 21:29:52 +0700

    adding realtime poll

--- app/models/PollingUser.php
+++ app/models/PollingUser.php
@@ -24,11 +24,17 @@ class PollingUser{
 
   public function pollInput($id = null, $user = null){
     if(!is_null($id) && !is_null($user)){
+      date_default_timezone_set('Asia/Jakarta');
+      $date = date("Y-m-d H:i:s");
       $query = "UPDATE polling SET value=value+1 WHERE id=:id";
       $this->db->query($query);
       $this->db->bind('id', $id);
       $this->db->execute();
-      var_dump($this->catatUser($user));
+
+      $query_date = "UPDATE polling SET date='$date'";
+      $this->db->query($query_date);
+      $this->db->execute();
+      $this->catatUser($user);
       return $this->db->rowCount();
     }
   }
@@ -45,5 +51,55 @@ class PollingUser{
     $this->db->bind('id', $id);
     $this->db->execute();
     return ($this->db->rowCount() > 0) ? true : null;
+  }
+
+  public function getPollRealtime($ajax_call = null){
+    if(!is_null($ajax_call)){
+      while(true){
+
+        $last_ajax_call= isset($ajax_call) ? (int)$ajax_call : null;
+
+        clearstatcache();
+
+        $query = "SELECT value,date FROM polling";
+        $this->db->query($query);
+        $data = $this->db->resultAll();
+        $waktu = strtotime($data[0]['date']);
+
+        if($last_ajax_call == null || $waktu > $last_ajax_call){
+
+          // var_dump($data[0]);
+          $value = json_encode($data[0]['value']);
+          $result = array(
+            'value' => json_decode($value),
+            'timestamp' => $waktu
+          );
+
+          $json = json_encode($result);
+          echo $json;
+
+          // foreach($data as $val){
+          //   $value = json_encode($val['value']);
+          //
+          //   $result = array(
+          //     'value' => json_decode($value),
+          //     'timestamp' => $waktu
+          //   );
+          //
+          //   $json = json_encode($result);
+          //   echo $json;
+          // }
+
+          break;
+        }else{
+
+          sleep(1);
+          continue;
+
+        }
+
+      }
+    }
+
   }
 }
