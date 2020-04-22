ebdeb12850f45a88d16019f5d506c73b38d02901
SQL47 <ajidalmajid6@gmail.com>
2020-04-22 21:30:09 +0700

    update js script

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,6 +1,7 @@
 </div>
 </div>
 <script type="text/javascript" src="<?= BASEURL; ?>/public/js/script.js"></script>
+<script type="text/javascript" src="<?= BASEURL; ?>/public/js/poll.js"></script>
 <script type="text/javascript" src="<?= BASEURL; ?>/public/js/menubar.js"></script>
 </body>
 </html>
