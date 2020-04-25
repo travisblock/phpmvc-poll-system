fcc5f8568503ca7860118a353a327843c47cf0e0
SQL47 <ajidalmajid6@gmail.com>
2020-04-25 20:44:26 +0700

    update index

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -5,6 +5,7 @@
 
 <div class="row-box">
   <?php
+  if(!empty($data)){
   foreach($data['poll'] as $key){
   ?>
   <div class="pool-card">
@@ -21,28 +22,6 @@
 
   <?php
   }
+  }
   ?>
-
-  <!-- <div class="pool-card">
-    <h3>CodeIgniter</h3>
-    <span class="text-suara">30 suara</span>
-    <div class="pool-bar">
-      <span class="span1">
-        <span class="span2" style="width:30%">
-          <span class="span3">30%</span>
-        </span>
-      </span>
-    </div>
-  </div>
-  <div class="pool-card">
-    <h3>Symfony</h3>
-    <span class="text-suara">20 suara</span>
-    <div class="pool-bar">
-      <span class="span1">
-        <span class="span2" style="width:20%">
-          <span class="span3">20%</span>
-        </span>
-      </span>
-    </div>
-  </div> -->
 </div>
