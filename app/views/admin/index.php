b088ff7e0479b49121b60d2ee6e5caff8adf4148
SQL47 <ajidalmajid6@gmail.com>
2020-04-25 20:44:11 +0700

    index

---
+++ app/views/admin/index.php
@@ -0,0 +1,5 @@
+<form method="POST" action="<?= BASEURL;?>/admin/loginad">
+  <input type="text" name="username">
+  <input type="text" name="password">
+  <input type="submit" value="submit">
+</form>
