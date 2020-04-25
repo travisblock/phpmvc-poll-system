353161118bf73418532f419159a04d971e06dadd
SQL47 <ajidalmajid6@gmail.com>
2020-04-25 20:43:41 +0700

    dashboard

---
+++ app/views/admin/dashboard.php
@@ -0,0 +1,18 @@
+<div class="container">
+  <div class="content">
+    <h4><?= $data['email']; ?></h4>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus provident animi dolor in laudantium consectetur corporis iusto! At recusandae maxime optio commodi aliquid velit consequatur ipsa sit! Veniam, fugiat. Provident assumenda perspiciatis, neque illo ut veritatis esse obcaecati vero! Molestiae accusantium excepturi ex consequatur. Deleniti dolore repudiandae impedit cum, ad amet fuga iusto, totam velit! Repudiandae ut laborum veritatis facere odio adipisci aspernatur dolore neque voluptate sunt aliquid, itaque, perferendis natus omnis tenetur. Molestiae dolor, eos incidunt maiores aperiam cumque!
+  </div>
+
+  <div class="content">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus provident animi dolor in laudantium consectetur corporis iusto! At recusandae maxime optio commodi aliquid velit consequatur ipsa sit! Veniam, fugiat. Provident assumenda perspiciatis, neque illo ut veritatis esse obcaecati vero! Molestiae accusantium excepturi ex consequatur. Deleniti dolore repudiandae impedit cum, ad amet fuga iusto, totam velit! Repudiandae ut laborum veritatis facere odio adipisci aspernatur dolore neque voluptate sunt aliquid, itaque, perferendis natus omnis tenetur. Molestiae dolor, eos incidunt maiores aperiam cumque!
+  </div>
+
+  <div class="content">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus provident animi dolor in laudantium consectetur corporis iusto! At recusandae maxime optio commodi aliquid velit consequatur ipsa sit! Veniam, fugiat. Provident assumenda perspiciatis, neque illo ut veritatis esse obcaecati vero! Molestiae accusantium excepturi ex consequatur. Deleniti dolore repudiandae impedit cum, ad amet fuga iusto, totam velit! Repudiandae ut laborum veritatis facere odio adipisci aspernatur dolore neque voluptate sunt aliquid, itaque, perferendis natus omnis tenetur. Molestiae dolor, eos incidunt maiores aperiam cumque!
+  </div>
+
+  <div class="content">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus provident animi dolor in laudantium consectetur corporis iusto! At recusandae maxime optio commodi aliquid velit consequatur ipsa sit! Veniam, fugiat. Provident assumenda perspiciatis, neque illo ut veritatis esse obcaecati vero! Molestiae accusantium excepturi ex consequatur. Deleniti dolore repudiandae impedit cum, ad amet fuga iusto, totam velit! Repudiandae ut laborum veritatis facere odio adipisci aspernatur dolore neque voluptate sunt aliquid, itaque, perferendis natus omnis tenetur. Molestiae dolor, eos incidunt maiores aperiam cumque!
+  </div>
+</div>
