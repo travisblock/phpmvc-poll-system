40a469f5c965e60326c42acf76077c52c4d6464b
SQL47 <ajidalmajid6@gmail.com>
2020-04-26 17:02:38 +0700

    add view/admin/polling/tambah

---
+++ app/views/admin/polling/tambah.php
@@ -0,0 +1,9 @@
+<div class="container">
+  <div class="content">
+    <form method="POST" action="<?= BASEURL; ?>/admin/polling/tambah">
+      <input type="text" name="nama">
+      <input type="text" name="detail">
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+</div>
