5943ba0bb7c8aa4afd87e77cbc9f327031c01eb8
SQL47 <ajidalmajid6@gmail.com>
2020-04-26 16:59:55 +0700

    add more feature

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -54,14 +54,61 @@ class Admin extends Controller{
       }
   }
 
-  public function polling(){
+  public function polling($param1 = null, $param2 = null){
       session_start();
       if(Session::exists('username')){
         $data['judul'] = 'Polling';
-        $data['email'] = 'pollinggann';
-        $this->view('admin/header', $data);
-        $this->view('admin/polling', $data);
-        $this->view('admin/footer');
+        $data['polling'] = $this->model('PollingUser')->getPolling();
+
+        if(is_null($param1) && is_null($param2)){
+          $this->view('admin/header', $data);
+          $this->view('admin/polling/index', $data);
+          $this->view('admin/footer');
+
+        }elseif($param1 == 'tambah' && is_null($param2)){
+          $data['judul'] = 'Tambah Kandidat';
+          if(!empty($_POST)){
+            $data['nama'] = htmlentities($_POST['nama']);
+            $data['detail'] = htmlentities($_POST['detail']);
+            if($this->model('PollingControl')->tambah($data) > 0){
+              echo "<script>alert('Berhasil Tambah Kandidat'); window.location.href='".BASEURL."/admin/polling'</script>";
+            }else{
+              echo "<script>alert('GAGAL Tambah Kandidat'); window.location.href='".BASEURL."/admin/polling'</script>";
+            }
+          }
+
+          $this->view('admin/header', $data);
+          $this->view('admin/polling/tambah', $data);
+          $this->view('admin/footer');
+
+        }elseif($param1 == 'hapus' && !is_null($param2)){
+          if($this->model('PollingControl')->hapus($param2) > 0){
+            echo "<script>alert('Berhasil Hapus Kandidat'); window.location.href='".BASEURL."/admin/polling'</script>";
+          }else{
+            echo "<script>alert('GAGAL Hapus Kandidat'); window.location.href='".BASEURL."/admin/polling'</script>";
+          }
+
+        }elseif($param1 == 'edit' && !is_null($param2)){
+          $data['judul'] = 'Edit Kandidat';
+          $data['kandidat'] = $this->model('PollingUser')->getPollById($param2);
+          if(!empty($_POST)){
+            $data['nama'] = htmlentities($_POST['nama']);
+            $data['detail'] = htmlentities($_POST['detail']);
+            if($this->model('PollingControl')->edit($data, $param2) > 0){
+              echo "<script>alert('Berhasil Edit Kandidat'); window.location.href='".BASEURL."/admin/polling'</script>";
+            }else{
+              echo "<script>alert('GAGAL Edit Kandidat'); window.location.href='".BASEURL."/admin/polling'</script>";
+            }
+          }
+          $this->view('admin/header', $data);
+          $this->view('admin/polling/edit', $data);
+          $this->view('admin/footer');
+
+        }else{
+          header('Location:'. BASEURL . '/admin/polling');
+          exit();
+        }
+
       }else{
         header('Location:'. BASEURL . '/admin');
         exit();
