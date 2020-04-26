a88994c8364b39bd92c3b6f025723512d639f16d
SQL47 <ajidalmajid6@gmail.com>
2020-04-27 00:34:19 +0700

    add view tambah user

---
+++ app/views/admin/userman/tambah.php
@@ -0,0 +1,9 @@
+<div class="container">
+  <div class="content">
+    <form method="POST" action="<?= BASEURL; ?>/admin/userman/tambah">
+      <input type="text" name="username">
+      <input type="text" name="pass">
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+</div>
