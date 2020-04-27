b18d06c02605cf7282f7e922cd13ae26642a1a1c
SQL47 <ajidalmajid6@gmail.com>
2020-04-28 00:24:01 +0700

    update user, polling, setting, session

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -4,7 +4,7 @@ class Admin extends Controller{
 
   public function index(){
     session_start();
-    if(empty(Session::get('username'))){
+    if(empty(Session::get('AdminName'))){
       $this->view('admin/index');
     }else{
       header('Location:'. BASEURL .'/admin/dashboard');
@@ -21,7 +21,7 @@ class Admin extends Controller{
       if($data){
         session_start();
         Session::set('id', $data['id']);
-        Session::set('username', $data['user']);
+        Session::set('AdminName', $data['user']);
         Session::set('email', $data['email']);
 
         header('Location:'. BASEURL . '/admin/dashboard');
@@ -39,7 +39,7 @@ class Admin extends Controller{
 
   public function dashboard(){
       session_start();
-      if(Session::exists('username')){
+      if(Session::exists('AdminName')){
         $data['judul'] = 'Dashboard';
         $data['email'] = Session::get('email');
         $this->view('admin/header', $data);
@@ -62,24 +62,68 @@ class Admin extends Controller{
 
   public function polling($param1 = null, $param2 = null){
       session_start();
-      if(Session::exists('username')){
-        $data['judul'] = 'Polling';
+      if(Session::exists('AdminName')){
+        $data['judul']   = 'Polling';
         $data['polling'] = $this->model('PollingUser')->getPolling();
+
+        /**
+        *
+        * Untuk Menampilkan index polling manager
+        *
+        */
 
         if(is_null($param1) && is_null($param2)){
           $this->view('admin/header', $data);
           $this->view('admin/polling/index', $data);
           $this->view('admin/footer');
 
+          /**
+          *
+          * Untuk Tambah kandidat
+          *
+          */
+
         }elseif($param1 == 'tambah' && is_null($param2)){
           $data['judul'] = 'Tambah Kandidat';
-          if(!empty($_POST)){
-            $data['nama'] = htmlentities(Input::get('nama'));
+
+          if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['nama'])){
+
+            $data['nama']   = htmlentities(Input::get('nama'));
             $data['detail'] = htmlentities(Input::get('detail'));
-            if($this->model('PollingControl')->tambah($data) > 0){
-              echo "<script>alert('Berhasil Tambah Kandidat'); window.location.href='".BASEURL."/admin/polling'</script>";
+            $data['img_name']  = $_FILES['img']['name'];
+            $data['img_size']  = $_FILES['img']['size'];
+            $data['img_tmp']   = $_FILES['img']['tmp_name'];
+            $data['img_ext']   = pathinfo($data['img_name'], PATHINFO_EXTENSION);
+            $data['img_path']  = "public/img/";
+            $data['allow_ext'] = array('jpg', 'JPG', 'png', 'PNG', 'jpeg');
+            $data['new_name'] = Upload::rename($data['img_name']);
+
+
+            if(empty($data['img_name'])){
+
+              if($this->model('PollingControl')->tambah($data) > 0){
+                header('Location:'. BASEURL . '/admin/polling');
+                exit();
+              }else{
+                echo "<script>alert('GAGAL Tambah Kandidat'); window.location.href='".BASEURL."/admin/polling'</script>";
+              }
+
             }else{
-              echo "<script>alert('GAGAL Tambah Kandidat'); window.location.href='".BASEURL."/admin/polling'</script>";
+              if(in_array($data['img_ext'], $data['allow_ext'])){
+
+                if(move_uploaded_file($data['img_tmp'], $data['img_path'] . $data['new_name'])){
+
+                  if($this->model('PollingControl')->tambah($data) > 0){
+                    header('Location:'. BASEURL . '/admin/polling');
+                    exit();
+                  }else{
+                    echo "<script>alert('GAGAL Tambah Kandidat'); window.location.href='".BASEURL."/admin/polling'</script>";
+                  }
+
+                }
+              }else{
+                echo "<script>alert('Hanya boleh JPG dan PNG'); window.location.href='".BASEURL."/admin/polling/tambah'</script>";
+              }
             }
           }
 
@@ -87,28 +131,86 @@ class Admin extends Controller{
           $this->view('admin/polling/tambah', $data);
           $this->view('admin/footer');
 
+          /**
+          *
+          * Untuk Hapus kandidat
+          *
+          */
+
         }elseif($param1 == 'hapus' && !is_null($param2)){
+
+          $img = $this->model('PollingUser')->getPollById($param2);
+          if(is_file('public/img/'. $img['img'])){
+            unlink('public/img/'. $img['img']);
+          }
+
           if($this->model('PollingControl')->hapus($param2) > 0){
-            echo "<script>alert('Berhasil Hapus Kandidat'); window.location.href='".BASEURL."/admin/polling'</script>";
+            header('Location:'. BASEURL . '/admin/polling');
+            exit();
           }else{
             echo "<script>alert('GAGAL Hapus Kandidat'); window.location.href='".BASEURL."/admin/polling'</script>";
           }
 
+          /**
+          *
+          * Untuk Edit kandidat
+          *
+          */
+
         }elseif($param1 == 'edit' && !is_null($param2)){
-          $data['judul'] = 'Edit Kandidat';
+          $data['judul']    = 'Edit Kandidat';
           $data['kandidat'] = $this->model('PollingUser')->getPollById($param2);
-          if(!empty($_POST)){
-            $data['nama'] = htmlentities($_POST['nama']);
+
+          if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['nama'])){
+            $data['nama']   = htmlentities($_POST['nama']);
             $data['detail'] = htmlentities($_POST['detail']);
-            if($this->model('PollingControl')->edit($data, $param2) > 0){
-              echo "<script>alert('Berhasil Edit Kandidat'); window.location.href='".BASEURL."/admin/polling'</script>";
+            $data['img_name']  = $_FILES['img']['name'];
+            $data['img_size']  = $_FILES['img']['size'];
+            $data['img_tmp']   = $_FILES['img']['tmp_name'];
+            $data['img_ext']   = pathinfo($data['img_name'], PATHINFO_EXTENSION);
+            $data['img_path']  = "public/img/";
+            $data['allow_ext'] = array('jpg', 'JPG', 'png', 'PNG', 'jpeg');
+            $data['new_name'] = Upload::rename($data['img_name']);
+
+            if(empty($data['img_name'])){
+              if($this->model('PollingControl')->edit($data, $param2) > 0){
+                header('Location:'. BASEURL . '/admin/polling');
+                exit();
+              }else{
+                echo "<script>alert('GAGAL edit Kandidat'); window.location.href='".BASEURL."/admin/polling/edit/". $data['kandidat']['id'] ."'</script>";
+              }
             }else{
-              echo "<script>alert('GAGAL Edit Kandidat'); window.location.href='".BASEURL."/admin/polling'</script>";
+
+              if(in_array($data['img_ext'], $data['allow_ext'])){
+
+                if(move_uploaded_file($data['img_tmp'], $data['img_path'] . $data['new_name'])){
+
+                  if(is_file('public/img/'. $data['kandidat']['img'])){
+                    unlink('public/img/'. $data['kandidat']['img']);
+                  }
+
+                  if($this->model('PollingControl')->edit($data, $param2) > 0){
+                    header('Location:'. BASEURL . '/admin/polling');
+                    exit();
+                  }else{
+                    echo "<script>alert('GAGAL edit Kandidat'); window.location.href='".BASEURL."/admin/polling/edit/". $data['kandidat']['id'] ."'</script>";
+                  }
+                }
+
+              }else{
+                echo "<script>alert('Hanya boleh JPG dan PNG'); window.location.href='".BASEURL."/admin/polling/edit/". $data['kandidat']['id'] ."'</script>";
+              }
             }
           }
-          $this->view('admin/header', $data);
-          $this->view('admin/polling/edit', $data);
-          $this->view('admin/footer');
+
+          if($data['kandidat']){
+            $this->view('admin/header', $data);
+            $this->view('admin/polling/edit', $data);
+            $this->view('admin/footer');
+          }else{
+            header('Location:'. BASEURL . '/admin/polling');
+            exit();
+          }
 
         }else{
           header('Location:'. BASEURL . '/admin/polling');
@@ -133,9 +235,15 @@ class Admin extends Controller{
 
   public function userman($param1 = null, $param2 = null){
       session_start();
-      if(Session::exists('username')){
+      if(Session::exists('AdminName')){
         $data['judul'] = 'User Manager';
-        $data['user'] = $this->model('UserMan')->getUser();
+        $data['user']  = $this->model('UserMan')->getUser();
+
+        /**
+        *
+        * Untuk Menampilkan index user manager
+        *
+        */
 
         if(is_null($param1) && is_null($param2)){
           $this->view('admin/header', $data);
@@ -143,16 +251,13 @@ class Admin extends Controller{
           $this->view('admin/footer');
 
           /**
-          * USERMAN - User Manager for manage user
-          * function userman for tambah,edit,hapus user
           *
-          * @param param1 used to action ex: tambah
-          * @param param2 used to id ex: 1 -> /admin/polling/tambah/1
+          * Untuk Tambah User
           *
           */
 
         }elseif($param1 == 'tambah' && is_null($param2)){
-          $data['judul']    = 'Tambah User';
+          $data['judul'] = 'Tambah User';
 
           if(!empty($_POST)){
             $data['username'] = htmlentities(Input::get('username'));
@@ -170,6 +275,11 @@ class Admin extends Controller{
           $this->view('admin/userman/tambah', $data);
           $this->view('admin/footer');
 
+          /**
+          *
+          * Untuk Hapus User
+          *
+          */
 
         }elseif($param1 == 'hapus' && !is_null($param2)){
           if($this->model('UserMan')->hapus($param2) > 0){
@@ -179,11 +289,37 @@ class Admin extends Controller{
             echo "<script>alert('GAGAL Hapus User'); window.location.href='".BASEURL."/admin/userman'</script>";
           }
 
+          /**
+          *
+          * Untuk Edit User
+          *
+          */
+
         }elseif($param1 == 'edit' && !is_null($param2)){
           $data['judul'] = 'Edit User';
-          $this->view('admin/header', $data);
-          $this->view('admin/userman/index', $data);
-          $this->view('admin/footer');
+          $data['user']  = $this->model('UserMan')->getUserById($param2);
+
+          if(!empty($_POST)){
+            $data['username'] = htmlentities($_POST['username']);
+            $data['pass']     = $_POST['pass'];
+
+            if($this->model('UserMan')->edit($data, $param2) > 0){
+              header('Location:'. BASEURL . '/admin/userman');
+              exit();
+            }else{
+              echo "<script>alert('GAGAL Edit User'); window.location.href='".BASEURL."/admin/userman'</script>";
+            }
+          }
+
+          if($data['user']){
+            $this->view('admin/header', $data);
+            $this->view('admin/userman/edit', $data);
+            $this->view('admin/footer');
+          }else{
+            header('Location:'. BASEURL . '/admin/userman');
+            exit();
+          }
+
         }else{
           header('Location:'. BASEURL . '/admin/userman');
           exit();
@@ -197,9 +333,9 @@ class Admin extends Controller{
 
   public function setting(){
       session_start();
-      if(Session::exists('username')){
+      if(Session::exists('AdminName')){
         $data['judul'] = 'Setting Manager';
-        $data['email'] = Session::get('username');
+        $data['email'] = Session::get('AdminName');
         $this->view('admin/header', $data);
         $this->view('admin/dashboard', $data);
         $this->view('admin/footer');
