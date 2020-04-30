4b4323ae391cce67cd66c83db50650ec60817de7
SQL47 <ajidalmajid6@gmail.com>
2020-04-30 22:15:12 +0700

    add mass delete poll with checkbox

--- app/views/admin/userman/index.php
+++ app/views/admin/userman/index.php
@@ -5,38 +5,42 @@
   <div class="content">
     <a class="tbl-aksi tbl-add" href="<?= BASEURL; ?>/admin/userman/tambah">+ tambah user</a>
     <div class="tabel-responsive">
-      <table class="tabel">
+      <form id="massDelete" style="width:100%" method="POST" action="<?= BASEURL; ?>/admin/userman/massdelete">
+        <table class="tabel">
+          <thead>
+            <tr>
+              <th width="5%"><input type="checkbox" id="btnCheckHapus" onclick="checkAll()"></th>
+              <th width="10%">No.</th>
+              <th width="45%">Username</th>
+              <th width="20%">Status</th>
+              <th width="20%">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+              if(!empty($data['user'])){
+                $no = 1;
+                foreach($data['user'] as $user){
 
-        <thead>
-          <tr>
-            <th width="10%">No.</th>
-            <th width="50%">Username</th>
-            <th width="20%">Status</th>
-            <th width="20%">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-            if(!empty($data['user'])){
-              $no = 1;
-              foreach($data['user'] as $user){
-
-          ?>
-          <tr class="<?php echo ($user['sudah'] > 0) ? 'sudah' : "belum" ?>" >
-            <td width="10%"><?= $no; ?></td>
-            <td width="50%"><?= $user['username']; ?></td>
-            <td width="20%"><?php echo ($user['sudah'] > 0) ? 'Sudah' : "Belum" ?></td>
-            <td width="20%"><a class="tbl-aksi tbl-hapus" href="<?= BASEURL . '/admin/userman/hapus/' . $user['id']; ?>" onclick="return confirm('Yakin akan menghapus user ?')">Hapus</a>&nbsp;
-                <a class="tbl-aksi tbl-edit" href="<?= BASEURL . '/admin/userman/edit/' . $user['id']; ?>">Edit</a>
-            </td>
-          </tr>
-          <?php
-              $no++;
+            ?>
+            <tr class="<?php echo ($user['sudah'] > 0) ? 'sudah' : "belum" ?>" >
+              <td width="5%"><input id="checkHapus" type="checkbox" name="hapusU[]" value="<?= $user['id']; ?>"></td>
+              <td width="10%"><?= $no; ?></td>
+              <td width="45%"><?= $user['username']; ?></td>
+              <td width="20%"><?php echo ($user['sudah'] > 0) ? 'Sudah' : "Belum" ?></td>
+              <td width="20%"><a class="tbl-aksi tbl-hapus" href="<?= BASEURL . '/admin/userman/hapus/' . $user['id']; ?>" onclick="return confirm('Yakin akan menghapus user ?')">Hapus</a>&nbsp;
+                  <a class="tbl-aksi tbl-edit" href="<?= BASEURL . '/admin/userman/edit/' . $user['id']; ?>">Edit</a>
+              </td>
+            </tr>
+            <?php
+                $no++;
+                }
               }
-            }
-          ?>
-        </tbody>
-      </table>
+            ?>
+          </tbody>
+        </table>
+        <input type="submit" class="tbl-aksi tbl-del" value="Mass Delete">
+      </form>
     </div>
   </div>
 
