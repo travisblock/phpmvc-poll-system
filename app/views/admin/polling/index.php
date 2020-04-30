34c70c32524d7668393152c25b1847fbf718e7c9
SQL47 <ajidalmajid6@gmail.com>
2020-04-30 22:14:49 +0700

    mass delete user with checkbox

--- app/views/admin/polling/index.php
+++ app/views/admin/polling/index.php
@@ -5,38 +5,43 @@
   <div class="content">
     <a class="tbl-aksi tbl-add" href="<?= BASEURL; ?>/admin/polling/tambah">+ tambah kandidat</a>
     <div class="tabel-responsive">
-      <table class="tabel">
+      <form id="massDelete" style="width:100%" method="POST" action="<?= BASEURL; ?>/admin/polling/massdelete">
+        <table class="tabel">
 
-        <thead>
-          <tr>
-            <th>No.</th>
-            <th>Nama</th>
-            <th>Detail</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-            if(!empty($data['polling'])){
-              $no = 1;
-              foreach($data['polling'] as $poll){
+          <thead>
+            <tr>
+              <th><input type="checkbox" id="btnCheckHapus" onclick="checkAll()"></th>
+              <th>No.</th>
+              <th>Nama</th>
+              <th>Detail</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+              if(!empty($data['polling'])){
+                $no = 1;
+                foreach($data['polling'] as $poll){
 
-          ?>
-          <tr>
-            <td><?= $no; ?></td>
-            <td><?= $poll['name']; ?></td>
-            <td><?= substr($poll['detail'], 0, 90); ?>...</td>
-            <td><a class="tbl-aksi tbl-hapus" href="<?= BASEURL . '/admin/polling/hapus/' . $poll['id']; ?>" onclick="return confirm('Yakin akan menghapus?\nIni juga menghapus fotonya');">Hapus</a>&nbsp;
-                <a class="tbl-aksi tbl-edit" href="<?= BASEURL . '/admin/polling/edit/' . $poll['id']; ?>">Edit</a>
-            </td>
-          </tr>
-          <?php
-              $no++;
+            ?>
+            <tr>
+              <td width="5%"><input id="checkHapus" type="checkbox" name="hapusK[]" value="<?= $poll['id']; ?>"></td>
+              <td><?= $no; ?></td>
+              <td><?= $poll['name']; ?></td>
+              <td><?= substr($poll['detail'], 0, 90); ?>...</td>
+              <td><a class="tbl-aksi tbl-hapus" href="<?= BASEURL . '/admin/polling/hapus/' . $poll['id']; ?>" onclick="return confirm('Yakin akan menghapus?\nIni juga menghapus fotonya');">Hapus</a>&nbsp;
+                  <a class="tbl-aksi tbl-edit" href="<?= BASEURL . '/admin/polling/edit/' . $poll['id']; ?>">Edit</a>
+              </td>
+            </tr>
+            <?php
+                $no++;
+                }
               }
-            }
-          ?>
-        </tbody>
-      </table>
+            ?>
+          </tbody>
+        </table>
+        <input type="submit" class="tbl-aksi tbl-del" value="Mass Delete">
+      </form>
     </div>
   </div>
 
