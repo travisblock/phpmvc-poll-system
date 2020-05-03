8f6484686cfdc600a4b1f8db95d057d8efb23c52
SQL47 <ajidalmajid6@gmail.com>
2020-05-03 23:49:02 +0700

    menambahkan session_start

--- app/init.php
+++ app/init.php
@@ -4,6 +4,8 @@ spl_autoload_register(function($class){
   require_once 'app/core/'.$class.'.php';
 });
 
+session_start();
+
 require_once 'app/config/config.php';
 
 require_once 'library/vendor/autoload.php';
