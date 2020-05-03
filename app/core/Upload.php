8eadc135afc3e24d0ce35aaed3f7ff770dd3e54f
SQL47 <ajidalmajid6@gmail.com>
2020-05-03 23:47:43 +0700

    memperbaiki upload

--- app/core/Upload.php
+++ app/core/Upload.php
@@ -3,14 +3,63 @@
 
 class Upload{
 
+  private $name,
+          $path,
+          $tmp,
+          $ext,
+          $newName,
+          $allowed;
 
-  public static function rename($name = null){
-    if(!empty($name)){
-      $tmp_name         = explode(".", $name);
-      $__name           = $tmp_name[0];
-      $ext_name         = strtolower(end($tmp_name));
-      $new_name = round(microtime(true)) . '_' . $__name . '.' . $ext_name;
-      return $new_name;
+  public function __construct(array $files = [], string $path = null){
+    $this->name = $files['name'];
+    $this->tmp  = $files['tmp_name'];
+    $this->ext  = pathinfo($this->name, PATHINFO_EXTENSION);
+    $this->path = $path;
+  }
+
+  public function uploaded(string $type){
+    $this->rename();
+    if($this->allowed($type)){
+      return (move_uploaded_file($this->tmp, $this->path . $this->newName)) ? true : false;
+    }else{
+      die('Error: ekstensi file salah');
     }
   }
+
+  public function rename(){
+    $this->newName = explode(".", $this->name);
+    $this->ext     = strtolower(end($this->newName));
+    $this->newName = $this->newName[0];
+    $this->newName = round(microtime(true)) . '_' . $this->newName . '.' . $this->ext;
+    return $this->newName;
+  }
+
+  public function tmpName(){
+    return $this->tmp;
+  }
+
+  public function getNewName(){
+    return $this->newName;
+  }
+
+  private function allowExt(string $type){
+    switch ($type) {
+      case 'images':
+        $this->allowed = array('jpg', 'png', 'jpeg');
+        break;
+      case 'xls':
+        $this->allowed = array('xls', 'xlsx');
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  public function allowed(string $type){
+    $this->allowExt($type);
+    return (in_array($this->ext, $this->allowed)) ? true : false;
+  }
+
+
 }
