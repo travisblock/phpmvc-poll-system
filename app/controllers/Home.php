7d24bd87c08fe221bee29928904601f2a05b93a8
SQL47 <ajidalmajid6@gmail.com>
2020-05-04 17:33:08 +0700

    Memperbaiki nama model

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,7 +3,7 @@
 class Home extends Controller{
   public function index(){
     $data['judul'] = 'Sistem Polling Realtime';
-    $data['poll'] = $this->model('PollingUser')->getPolling();
+    $data['poll'] = $this->model('PollingModel')->getPolling();
     $this->view('templates/header', $data);
     $this->view('home/index', $data);
     $this->view('templates/footer');
