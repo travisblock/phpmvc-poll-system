f7e229847f1f40034a3cf985be7c5dfdffc82a8d
SQL47 <ajidalmajid6@gmail.com>
2020-05-04 18:44:46 +0700

    Ganti nama

---
+++ app/models/AdminModel.php
@@ -0,0 +1,30 @@
+<?php
+
+class AdminModel{
+
+  private $db;
+
+  public function __construct(){
+    return $this->db  = Database::getDB();
+  }
+
+  public function login($username = null, $password = null){
+    if(!is_null($username) && !is_null($password)){
+      $query = "SELECT * FROM login WHERE user=:user";
+      $this->db->query($query);
+      $this->db->bind('user', $username);
+      $data = $this->db->result();
+      if($this->db->rowCount() > 0){
+        return ($this->cekPassword($password, $data['pass'])) ? $data : false;
+      }else{
+        return false;
+      }
+    }
+  }
+
+  public function cekPassword($input = null, $password = null){
+    if(!is_null($input) && !is_null($password)){
+      return (password_verify($input, $password)) ? true : false;
+    }
+  }
+}
---
+++ app/models/PollingModel.php
@@ -0,0 +1,146 @@
+<?php
+
+class PollingModel{
+
+  private $db;
+
+  public function __construct(){
+    $this->db = Database::getDB();
+  }
+
+  public function tambah($data = null){
+
+    date_default_timezone_set('Asia/Jakarta');
+    $date = date("Y-m-d H:i:s");
+
+    if(!empty($data['nama'])){
+      if(!empty($data['new_name'])){
+        $query = "INSERT INTO polling(name,detail,value,img,date) VALUES(:nama, :detail, 0, '$data[new_name]', '$date')";
+      }else{
+        $query = "INSERT INTO polling(name,detail,value,img,date) VALUES(:nama, :detail, 0, 'default.png', '$date')";
+      }
+
+      $this->db->query($query);
+      $this->db->bind('nama', $data['nama']);
+      $this->db->bind('detail', $data['detail']);
+      $this->db->execute();
+
+      return $this->db->rowCount();
+    }
+  }
+
+  public function hapus($id = null){
+    $query = "DELETE FROM polling WHERE id=:id";
+    $this->db->query($query);
+    $this->db->bind('id', $id);
+    $this->db->execute();
+    return $this->db->rowCount();
+  }
+
+  public function edit($data = null, $id = null){
+    $img = $data['new_name'];
+    if(!empty($img)){
+      $query = "UPDATE polling set name=:nama, detail=:detail, img='$img' WHERE id=:id";
+    }else{
+      $query = "UPDATE polling set name=:nama, detail=:detail WHERE id=:id";
+    }
+    $this->db->query($query);
+    $this->db->bind('id', $id);
+    $this->db->bind('nama', $data['nama']);
+    $this->db->bind('detail', $data['detail']);
+    $this->db->execute();
+
+    return $this->db->rowCount();
+  }
+
+
+  public function getPolling(){
+    $query = "SELECT * FROM polling";
+    $this->db->query($query);
+    return $this->db->resultAll();
+  }
+
+  public function getPollById($id){
+    $query = "SELECT * FROM polling WHERE id=:id";
+    $this->db->query($query);
+    $this->db->bind('id', $id);
+
+    return $this->db->result();
+  }
+
+  public function pollInput($idpoll = null, $iduser = null){
+    if(!empty($idpoll) && !empty($iduser)){
+      date_default_timezone_set('Asia/Jakarta');
+      $date = date("Y-m-d H:i:s");
+      $query = "UPDATE polling SET value=value+1 WHERE id=:id";
+      $this->db->query($query);
+      $this->db->bind('id', $idpoll);
+      $this->db->execute();
+
+      $query_date = "UPDATE polling SET date='$date'";
+      $this->db->query($query_date);
+      $this->db->execute();
+      $this->catatUser($iduser);
+      return $this->db->rowCount();
+    }
+  }
+
+  public function catatUser($iduser){
+
+    $query = "UPDATE user set sudah=sudah+1 WHERE id=:iduser";
+    $this->db->query($query);
+    $this->db->bind('iduser', $iduser);
+    $this->db->execute();
+    return ($this->db->rowCount() > 0) ? true : false;
+  }
+
+  public function getPollRealtime($ajax_call = null){
+    if(!is_null($ajax_call)){
+      while(true){
+
+        $last_ajax_call= isset($ajax_call) ? (int)$ajax_call : null;
+
+        clearstatcache();
+
+        $query = "SELECT value,date FROM polling";
+        $this->db->query($query);
+        $data = $this->db->resultAll();
+        $waktu = strtotime($data[0]['date']);
+
+        if($last_ajax_call == null || $waktu > $last_ajax_call){
+
+          // var_dump($data[0]);
+          $value = json_encode($data[0]['value']);
+          $result = array(
+            'value' => json_decode($value),
+            'timestamp' => $waktu
+          );
+
+          $json = json_encode($result);
+          echo $json;
+
+          // foreach($data as $val){
+          //   $value = json_encode($val['value']);
+          //
+          //   $result = array(
+          //     'value' => json_decode($value),
+          //     'timestamp' => $waktu
+          //   );
+          //
+          //   $json = json_encode($result);
+          //   echo $json;
+          // }
+
+          break;
+        }else{
+
+          sleep(1);
+          continue;
+
+        }
+
+      }
+    }
+
+  }
+}
