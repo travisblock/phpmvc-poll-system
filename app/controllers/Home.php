e455f13f54dd9f5efdf3587861b3e7aafa416b2c
SQL47 <ajidalmajid6@gmail.com>
2020-05-05 16:43:40 +0700

    memperbaiki view home

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,7 +2,11 @@
 
 class Home extends Controller{
   public function index(){
-    $data['judul'] = 'Sistem Polling Realtime';
+    $data['view'] = $this->model('Settings')->getTampilan();
+
+    $data['judul_web'] = $data['view']['judul_web'];
+    $data['judul_voting'] = $data['view']['judul_voting'];
+    $data['deskripsi']  = $data['view']['deskripsi'];
     $data['poll'] = $this->model('PollingModel')->getPolling();
     $this->view('templates/header', $data);
     $this->view('home/index', $data);
