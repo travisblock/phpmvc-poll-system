9d08e5c3eae1b3d813f8cede3772b7c9b604bef1
SQL47 <ajidalmajid6@gmail.com>
2020-05-05 16:42:25 +0700

    deleted

--- app/views/admin/index.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="<?= BASEURL;?>/admin/loginad">
-  <input type="text" name="username">
-  <input type="text" name="password">
-  <input type="submit" value="submit">
-</form>
