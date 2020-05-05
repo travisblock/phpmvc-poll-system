a1e8707f97f434a25165d81749692cbe8579077d
SQL47 <ajidalmajid6@gmail.com>
2020-05-05 16:45:33 +0700

    memperbaiki model setting

--- app/models/Settings.php
+++ app/models/Settings.php
@@ -9,16 +9,17 @@ class Settings{
   }
 
   public function tampilan($data){
-    $query = "UPDATE pengaturan SET judul=:judul, deskripsi=:deskripsi";
+    $query = "UPDATE pengaturan SET judul_web=:judul_web, judul_voting=:judul_voting, deskripsi=:deskripsi";
     $this->db->query($query);
-    $this->db->bind('judul', $data['title']);
+    $this->db->bind('judul_web', $data['judul_web']);
+    $this->db->bind('judul_voting', $data['judul_voting']);
     $this->db->bind('deskripsi', $data['desc']);
     $this->db->execute();
 
     return ($this->db->rowCount() > 0) ? true : false;
   }
 
-  public function getTamplan(){
+  public function getTampilan(){
     $query = "SELECT * FROM pengaturan";
     $this->db->query($query);
     return $this->db->result();
