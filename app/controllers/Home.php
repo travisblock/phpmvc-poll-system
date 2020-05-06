d8da0421a8aa7bcd53a7f938322f1615cdf6574c
SQL47 <ajidalmajid6@gmail.com>
2020-05-06 22:18:55 +0700

    memperbaiki  judul, deskripsi, logo

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,10 +3,6 @@
 class Home extends Controller{
   public function index(){
     $data['view'] = $this->model('Settings')->getTampilan();
-
-    $data['judul_web'] = $data['view']['judul_web'];
-    $data['judul_voting'] = $data['view']['judul_voting'];
-    $data['deskripsi']  = $data['view']['deskripsi'];
     $data['poll'] = $this->model('PollingModel')->getPolling();
     $this->view('templates/header', $data);
     $this->view('home/index', $data);
