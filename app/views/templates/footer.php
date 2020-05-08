3a5ee59bd5a9af52d57bb68a664a3c8c650a80be
SQL47 <ajidalmajid6@gmail.com>
2020-05-09 05:51:18 +0700

    finishing view/templates/footer

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,7 +1,6 @@
 </div>
 </div>
+<script type="text/javascript">var baseurl = "<?= BASEURL; ?>";</script>
 <script type="text/javascript" src="<?= BASEURL; ?>/public/js/script.js"></script>
-<script type="text/javascript" src="<?= BASEURL; ?>/public/js/poll.js"></script>
-<script type="text/javascript" src="<?= BASEURL; ?>/public/js/menubar.js"></script>
 </body>
 </html>
