e4e2eee52384326313b015ed0a2184bfa1afdef6
SQL47 <ajidalmajid6@gmail.com>
2020-05-08 23:40:10 +0700

    memberi komentar

--- app/controllers/Polling.php
+++ app/controllers/Polling.php
@@ -3,24 +3,10 @@
 class Polling extends Controller{
 
   public function index(){
-    header('Location:'. BASEURL);
-    exit();
+    Redirect::to(BASEURL);
   }
 
-  public function getPollingById(){
-      $id = json_decode(file_get_contents('php://input'), true);
-      if(!empty($id)){
-        if(!is_null($id)){
-          foreach($id as $key){
-            $data = $this->model('PollingModel')->getPollById($key);
-            $this->view('user/polling', $data);
-          }
-        }
-      }else{
-        header('Location:'. BASEURL);
-        exit();
-      }
-  }
+
 
   public function pilih(){
     session_start();
@@ -31,31 +17,49 @@ class Polling extends Controller{
     if($user['sudah'] < 1){
       if($this->model('PollingModel')->pollInput($idpoll, $iduser) > 0){
         Session_destroy();
-        header('Location:'. BASEURL);
-        exit();
+        Redirect::to(BASEURL);
       }else{
-        header('Location:'. BASEURL);
-        exit();
+        Redirect::to(BASEURL);
       }
     }else{
       Session_destroy();
-      header('Location:'. BASEURL);
-      exit();
+      Redirect::to(BASEURL);
     }
   }
 
-  public function getPollingData(){
 
-    // stuck di sini
+  /**
+  *
+  * Mencoba untuk realtime tapi gagal, akan saya coba kembangkan terus
+  *
+  */
 
-    $data['judul'] = 'OK';
-    $this->view('templates/header', $data);
-    $id = json_decode(file_get_contents('php://input'), true);
-    $time = isset($id) ? (int)$id : null;
-    var_dump($id);
-    set_time_limit(0);
-    $this->model('PollingUser')->getPollRealtime($time);
-    $this->view('templates/footer');
-  }
+  // public function getPollingById(){
+  //     $id = json_decode(file_get_contents('php://input'), true);
+  //     if(!empty($id)){
+  //       if(!is_null($id)){
+  //         foreach($id as $key){
+  //           $data = $this->model('PollingModel')->getPollById($key);
+  //           $this->view('user/polling', $data);
+  //         }
+  //       }
+  //     }else{
+  //       header('Location:'. BASEURL);
+  //       exit();
+  //     }
+  // }
+  //
+  //
+  // public function getPollingData(){
+  //
+  //   $data['judul'] = 'OK';
+  //   $this->view('templates/header', $data);
+  //   $id = json_decode(file_get_contents('php://input'), true);
+  //   $time = isset($id) ? (int)$id : null;
+  //   var_dump($id);
+  //   set_time_limit(0);
+  //   $this->model('PollingUser')->getPollRealtime($time);
+  //   $this->view('templates/footer');
+  // }
 
 }
