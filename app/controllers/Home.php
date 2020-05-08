6557e764339a37beba2fb5f02c5265de24f166cd
SQL47 <ajidalmajid6@gmail.com>
2020-05-09 05:44:29 +0700

    finishing home controller

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+* @author Yusuf Al Majid <ajidalmajid6@gmail.com>
+* @license MIT
+* @version 0.0.1
+*/
+
+
 class Home extends Controller{
   public function index(){
     $data['view'] = $this->model('Settings')->getTampilan();
