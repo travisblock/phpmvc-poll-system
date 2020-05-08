dd4eebba99cf023cb2c8cc69a4bf5ae3d5821c1f
SQL47 <ajidalmajid6@gmail.com>
2020-05-08 23:36:32 +0700

    menambahkan constant untuk reset password

--- app/config/config.php
+++ app/config/config.php
@@ -5,3 +5,10 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
 define('DB_NAME', 'polling');
+
+// Untuk reset Passwd
+
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_USER', 'ajidgans69@gmail.com');
+define('SMTP_PASS', '#orapasswot$');
+define('SMTP_PORT', '587');
