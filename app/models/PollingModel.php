6551fb449678faecba9e5a4c980a3d212e52f712
SQL47 <ajidalmajid6@gmail.com>
2020-05-08 23:42:03 +0700

    memberi komentar

--- app/models/PollingModel.php
+++ app/models/PollingModel.php
@@ -94,53 +94,62 @@ class PollingModel{
     return ($this->db->rowCount() > 0) ? true : false;
   }
 
-  public function getPollRealtime($ajax_call = null){
-    if(!is_null($ajax_call)){
-      while(true){
 
-        $last_ajax_call= isset($ajax_call) ? (int)$ajax_call : null;
+  /**
+  *
+  * Mencoba untuk realtime tapi gagal, akan saya coba kembangkan terus
+  *
+  */
 
-        clearstatcache();
 
-        $query = "SELECT value,date FROM polling";
-        $this->db->query($query);
-        $data = $this->db->resultAll();
-        $waktu = strtotime($data[0]['date']);
+  // public function getPollRealtime($ajax_call = null){
+  //   if(!is_null($ajax_call)){
+  //     while(true){
+  //
+  //       $last_ajax_call= isset($ajax_call) ? (int)$ajax_call : null;
+  //
+  //       clearstatcache();
+  //
+  //       $query = "SELECT value,date FROM polling";
+  //       $this->db->query($query);
+  //       $data = $this->db->resultAll();
+  //       $waktu = strtotime($data[0]['date']);
+  //
+  //       if($last_ajax_call == null || $waktu > $last_ajax_call){
+  //
+  //         // var_dump($data[0]);
+  //         $value = json_encode($data[0]['value']);
+  //         $result = array(
+  //           'value' => json_decode($value),
+  //           'timestamp' => $waktu
+  //         );
+  //
+  //         $json = json_encode($result);
+  //         echo $json;
+  //
+  //         // foreach($data as $val){
+  //         //   $value = json_encode($val['value']);
+  //         //
+  //         //   $result = array(
+  //         //     'value' => json_decode($value),
+  //         //     'timestamp' => $waktu
+  //         //   );
+  //         //
+  //         //   $json = json_encode($result);
+  //         //   echo $json;
+  //         // }
+  //
+  //         break;
+  //       }else{
+  //
+  //         sleep(1);
+  //         continue;
+  //
+  //       }
+  //
+  //     }
+  //   }
+  //
+  // }
 
-        if($last_ajax_call == null || $waktu > $last_ajax_call){
-
-          // var_dump($data[0]);
-          $value = json_encode($data[0]['value']);
-          $result = array(
-            'value' => json_decode($value),
-            'timestamp' => $waktu
-          );
-
-          $json = json_encode($result);
-          echo $json;
-
-          // foreach($data as $val){
-          //   $value = json_encode($val['value']);
-          //
-          //   $result = array(
-          //     'value' => json_decode($value),
-          //     'timestamp' => $waktu
-          //   );
-          //
-          //   $json = json_encode($result);
-          //   echo $json;
-          // }
-
-          break;
-        }else{
-
-          sleep(1);
-          continue;
-
-        }
-
-      }
-    }
-
-  }
 }
